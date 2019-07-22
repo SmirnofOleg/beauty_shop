554f5e5ba985a180c9e8d14d4c19ceb091f40821
SmirnofOleg <super.smirnofoleg@yandex.ru>
2019-07-22 21:00:40 +0300

    добавил header и меню

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,37 @@
 $(function() {
+	var link = $('.menu-link');
+	var link_active = $('.menu-link_active');
 
-	// Custom JS
+	link.click(function(){
+		link.toggleClass('menu-link_active');
+		// menu.toggleClass('menu_active');
+	});
 
+	// стилизуем меню
+	$('#my-menu').mmenu({
+		extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black', 'position-right'],
+		navbar: {
+			title: '<a href="#" class="logo"><span>F</span>unny</a>'
+		},
+		
+	});
+	// добавили анимацию бургера
+	var api = $("#my-menu").data( "mmenu" );
+		api.bind( "open:finish", function( ) {
+		$('.menu-link').addClass('menu-link_active');
+		});
+		api.bind( "close:finish", function( ) {
+			$('.menu-link').removeClass('menu-link_active');
+			});
+			
+	//sticky headers
+	document.addEventListener(
+        "DOMContentLoaded", () => {
+            new Mhead( "#my-header", {
+                scroll: {
+                    hide: 200
+                }
+            });
+        }
+    );
 });
